98b15dbf33226779c8e6f8a78a8a7985ebd1cb6a
Vincent Kelleher <k2r79@hotmail.fr>
2015-04-01 09:03:04 +0200

    Corrected first scenario

--- features/step_definitions/user_questions_step.rb
+++ features/step_definitions/user_questions_step.rb
@@ -2,10 +2,17 @@ require 'cucumber/formatter/unicode' # Pour le support de l'UTF-8 pour Cucumber
 require 'watir-webdriver'
 
 browser = Watir::Browser.new :firefox
+number_of_questions = 0
 
 Soit /^un usager sur l'application web$/ do
   browser.goto 'file:///Users/Vincent/Workspace/Javascript/Question-Client/index.html'
   puts browser.url
+
+  browser.div(:id => 'questions').wait_until_present(5)
+
+  number_of_questions = browser.div(:id => 'questions')
+      .div(:id => 'accordion')
+      .divs.length
 end
 
 Lorsque(/^l'usager pose une question au serveur$/) do
@@ -19,6 +26,8 @@ Alors(/^le serveur indique qu'il a enregistré la question$/) do
 end
 
 Et(/^il permet à l'usager de localiser la réponse lorsqu'elle sera disponible$/) do
+  Watir::Wait.until(5) { number_of_questions == number_of_questions + 1 }
+
   browser.div(:id => 'questions')
     .div(:id => 'accordion')
     .div(:index => 0)
