de35ec878797c9029d3320ac0b11d7ba641c0944
Vincent Kelleher <k2r79@hotmail.fr>
2015-04-01 09:55:45 +0200

    Robust first scenario

--- features/step_definitions/user_questions_step.rb
+++ features/step_definitions/user_questions_step.rb
@@ -8,11 +8,12 @@ Soit /^un usager sur l'application web$/ do
   browser.goto 'file:///Users/Vincent/Workspace/Javascript/Question-Client/index.html'
   puts browser.url
 
-  browser.div(:id => 'questions').wait_until_present(5)
+  sleep 5
 
-  number_of_questions = browser.div(:id => 'questions')
-      .div(:id => 'accordion')
-      .divs.length
+  if browser.div(:id => 'questions').exists?
+    number_of_questions = browser.div(:id => 'questions')
+      .divs(:class => 'panel-group').length
+  end
 end
 
 Lorsque(/^l'usager pose une question au serveur$/) do
@@ -22,11 +23,11 @@ Lorsque(/^l'usager pose une question au serveur$/) do
 end
 
 Alors(/^le serveur indique qu'il a enregistré la question$/) do
-  Watir::Wait.until(5) { browser.div(:class => 'humane').text.include? 'Question enregistrée' }
+  Watir::Wait.until(10) { browser.div(:class => 'humane').text.include? 'Probléme' }
 end
 
 Et(/^il permet à l'usager de localiser la réponse lorsqu'elle sera disponible$/) do
-  Watir::Wait.until(5) { number_of_questions == number_of_questions + 1 }
+  Watir::Wait.until(10) { browser.div(:id => 'questions').exists? && browser.div(:id => 'questions').divs(:class => 'panel-group').length == number_of_questions + 1 }
 
   browser.div(:id => 'questions')
     .div(:id => 'accordion')
